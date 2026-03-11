8346065cdbf31be0f04bb940f1cb9b7aba89ed1a
Radosław Skałbania <radoslaw@cast.ai>
2026-03-11 08:25:05 +0100

    fix?: git status

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,8 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    update_debounce = 500,
+    watch_gitdir = { interval = 5000 },
+    attach_to_untracked = false,
+  },
+}
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,9 +1,17 @@
 return {
   {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
-      require('lualine').setup({})
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = function(_, opts)
+      local function gitsigns_source()
+        local gs = vim.b.gitsigns_status_dict
+        return gs and { added = gs.added, modified = gs.changed, removed = gs.removed }
+      end
+      -- Replace lualine's diff component with gitsigns-backed one
+      for _, section in ipairs(opts.sections.lualine_b or {}) do
+        if section[1] == "diff" or section == "diff" then section.source = gitsigns_source end
+      end
+      return opts
     end,
-  }
+  },
 }
